c1f013e3a4f4cc21ac5408b8ce954f7949d57d2d
xabdulla <abdullashaker002@gmail.com>
2025-06-29 16:35:17 +0300

    array lab

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ console.log('Exercise 5 result:', foods);
 
 foods.splice(1,1,'sushi', 'cupcake')
 console.log('Exercise 6 result:', foods);
+
+
+const yummy = foods.slice(1, 3);
+console.log('Exercise 7 result:', yummy);
+
+const soyIdx = foods.indexOf('tofu');
+console.log('Exercise 8 result:', soyIdx);
+
+const allFoods = foods.join(' -> ');
+console.log('Exercise 9 result:', allFoods);
